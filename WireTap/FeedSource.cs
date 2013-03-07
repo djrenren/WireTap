281c59170982f4a4a35eb37c61bc5e26fcb184d7
John Renner <john@jrenner.net>
2013-03-07 01:34:45 -0500

    Scope out FeedSource object

---
+++ WireTap/FeedSource.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WireTap
+{
+    class FeedSource : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public Uri Source { get; set; }
+        public DateTime LastUpdated { get; private set; }
+        private ObservableCollection<FeedItem> _Items { get; set; }
+        public ObservableCollection<FeedItem> Items
+        {
+            get
+            {
+                return _Items;
+            }
+        }
+
+        public FeedSource(string name, Uri source)
+        {
+            Name = name;
+            Source = source;
+        }
+
+        public void Update() {
+            _Items.Clear();
+
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(client_DownloadStringCompleted);
+            client.DownloadStringAsync(Source);
+
+        }
+
+        private void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+
+        }
+
+    }
+}
